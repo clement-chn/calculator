7d039b33a59aeb006d881d2cad57d0f73c877bb6
Clément Chouan <chouan.clement@gmail.com>
2024-01-26 18:28:39 +0100

    v1 du design terminé

--- src/App.jsx
+++ src/App.jsx
@@ -4,38 +4,41 @@ function App() {
 
   return (
     <>
-    <main>
-      <div>
-        <Button name="0" width="full"/>
-      </div>
-      <div>
-        <Button name="AC"/>
-        <Button name="+/-"/>
-        <Button name="%"/>
-        <Button name="÷" bgcolor="orange" textcolor="white"/>
-      </div>
-      <div>
-        <Button name="7"/>
-        <Button name="8"/>
-        <Button name="9"/>
-        <Button name="x" bgcolor="orange" textcolor="white"/>
-      </div>
-      <div>
-        <Button name="4"/>
-        <Button name="5"/>
-        <Button name="6"/>
-        <Button name="-" bgcolor="orange" textcolor="white"/>
-      </div>
-      <div>
-        <Button name="1"/>
-        <Button name="2"/>
-        <Button name="3"/>
-        <Button name="+" bgcolor="orange" textcolor="white"/>
-      </div>
-      <div>
-        <Button name="0" width="1/2"/>
-        <Button name=","/>
-        <Button name="=" bgcolor="orange" textcolor="white"/>
+    <main className="bg-black flex items-center justify-center h-screen">
+      <div> 
+      {/* je voulais revoir la taille ici ^ */}
+        <div>
+          <input type="text" className="w-full bg-white p-6 border border-black text-right outline-none"/>
+        </div>
+        <div>
+          <Button name="AC"/>
+          <Button name="+/-"/>
+          <Button name="%"/>
+          <Button name="÷" bgcolor="bg-orange-500" textcolor="text-white"/>
+        </div>
+        <div>
+          <Button name="7"/>
+          <Button name="8"/>
+          <Button name="9"/>
+          <Button name="x" bgcolor="bg-orange-500" textcolor="text-white"/>
+        </div>
+        <div>
+          <Button name="4"/>
+          <Button name="5"/>
+          <Button name="6"/>
+          <Button name="-" bgcolor="bg-orange-500" textcolor="text-white"/>
+        </div>
+        <div>
+          <Button name="1"/>
+          <Button name="2"/>
+          <Button name="3"/>
+          <Button name="+" bgcolor="bg-orange-500" textcolor="text-white"/>
+        </div>
+        <div>
+          <Button name="0" width="w-1/2"/>
+          <Button name=","/>
+          <Button name="=" bgcolor="bg-orange-500" textcolor="text-white"/>
+        </div>
       </div>
     </main>
     </>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,9 +4,9 @@ const Button = (props) => {
 
   return (
       <button className={`border 
-      ${props.width ? `w-${props.width}` :'w-1/4'} p-6 border-gray-300 
-      ${props.bgcolor ? `bg-${props.bgcolor}-500` :'bg-gray-200'}
-      ${props.textcolor ? `text-${props.textcolor}` :''}
+      ${props.width ? `${props.width}` : 'w-1/4'} p-6 border-black
+      ${props.bgcolor ? `${props.bgcolor}` :'bg-gray-200'}
+      ${props.textcolor ? `${props.textcolor}` :''}
       `}>
        {props.name}
       </button>
